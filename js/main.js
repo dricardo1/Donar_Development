fbbd0333baa98d8253f86e05ad30a9df6a762849
Don Ricardo JR <dricardo1@gmail.com>
2020-11-30 14:15:27 -0500

    Fix page ani slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".intro-text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 1 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.to(".intro", { y: "-100%", duration: .75 });
 tl.fromTo(".heading-text", { opacity: 0 }, { opacity: 1, duration: .5 });
-tl.fromTo(".first", { opacity: 0 }, { opacity: 1, duration: .5 });
+tl.fromTo(".first", { opacity: 0 }, { opacity: 1, duration: .25 });
 tl.fromTo(".second", { opacity: 0 }, { opacity: 1, duration: .25 });
-tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: .15 });
+tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: .25 });
 
 
--- js/work.js
+++ js/work.js
@@ -2,7 +2,6 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".large-text", { y: "0%", duration: .5 });
 tl.to(".work-text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 1.25 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".accent-text", { opacity: 0 }, { opacity: 1, duration: .25 });
-tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: .15 });
+tl.to(".intro", { y: "-100%", duration: 1 } );
+tl.fromTo(".accent-text", { opacity: 0 }, { opacity: 1, duration: .5 });
+tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: .75 });
