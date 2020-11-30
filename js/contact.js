cf5e4462248880b994b111b3375492404c95c085
Don Ricardo JR <dricardo1@gmail.com>
2020-11-30 11:32:58 -0500

    Clear extraneous gsap tl

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".large-text", { y: "0%", duration: 1 });
 tl.to(".intro-text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1, delay: 1 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
--- js/work.js
+++ js/work.js
@@ -1,11 +1,8 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-
 tl.to(".large-text", { y: "0%", duration: .5 });
-tl.to(".intro-text", { y: "0%", duration: .5, stagger: 0.25 });
 tl.to(".work-text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1, delay: 1.25 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".heading-text", { opacity: 0 }, { opacity: 1, duration: .5 });
 tl.fromTo(".accent-text", { opacity: 0 }, { opacity: 1, duration: .25 });
 tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: .15 });
