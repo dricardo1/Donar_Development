4851f6fd6fc54359bdee95a1ab5ada6fb5a92858
Don Ricardo JR <dricardo1@gmail.com>
2020-12-11 11:18:28 -0500

    UX nav refactor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".intro-text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".intro", { y: "-999%", duration: 1, delay: 1, opacity: 0 });
+tl.to(".intro", { y: "-999%", duration: 1, delay: 1.25, opacity: 0 });
 tl.fromTo(".heading-text", { opacity: 0 }, { opacity: 1, duration: .5 });
 tl.fromTo(".first", { opacity: 0 }, { opacity: 1, duration: .25 }, "-=.35");
 tl.fromTo(".second", { opacity: 0 }, { opacity: 1, duration: .25 }, "-=.25");
