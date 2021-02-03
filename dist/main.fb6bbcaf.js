e3a37c65542b5dcd552347e5fb2902e572884155
Don Ricardo JR <dricardo1@gmail.com>
2021-02-03 15:01:29 -0500

    contact jank fix

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -186,7 +186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55164" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57498" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
