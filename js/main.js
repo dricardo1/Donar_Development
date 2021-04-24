5cec0aafc56d3324cb634b9c39eda0da15893ec0
Don Ricardo JR <dricardo1@gmail.com>
2021-04-24 17:11:13 -0400

    UX updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".intro-text", { y: "0%", duration: .5, stagger: 0.25 });
+tl.to(".intro-text", { y: "0%", duration: 1.5, stagger: .15 });
 tl.to(".intro", { y: "-999%", duration: .5, delay: .5, opacity: 0 });
 tl.fromTo(".heading-text", { opacity: 0 }, { opacity: 1, duration: .5 });
 tl.fromTo(".first", { opacity: 0 }, { opacity: 1, duration: .25 }, "-=.25");
